eb12d44dffadf0c62fe231926a5004e5ef58d1a4
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2014-11-13 16:46:37 -0600

    Add the setup script
    The setup script has been added with some basic information.

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "ccpoviz",
+    version = "0.0.1",
+    packages = find_packages(),
+    scripts = [],
+
+    install_requires = [
+        'docutils>=0.3',
+        'pystache>=0.5',
+        ],
+
+    package_data = {
+        'ccpoviz': ['data/*.json', 'data/*.dat'],
+    },
+
+    # metadata for upload to PyPI
+    author = "Tschijnmo TSCHAU",
+    author_email = "tschijnmotschau@gmail.com",
+    description = "Command-line batch molecular visualizer",
+    license = "MIT",
+    keywords = "visualization, chemistry",
+    url = "http://tschijnmo.github.io/ccpoviz",
+
+)

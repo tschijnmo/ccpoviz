1ba696a2c0417edb51ff350149ff91a541c06ada
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2014-11-18 19:39:47 -0600

    Rename the readmol module
    The module, and its content, has been renamed that the ``mol`` are all
    changed into ``structure`` for better consistency with other parts of
    the code and future extension to periodic systems.

--- ccpoviz/readstructure.py
+++ ccpoviz/readstructure.py
@@ -1,18 +1,18 @@
 """
-Read molecules from input file
-==============================
+Read structures from input file
+===============================
 
-This module contains driver function for reading a molecule from a given input
-file with a given reader. The reader is given as a string.
+This module contains driver function for reading a molecular structures from a
+given input file with a given reader. The reader is given as a string.
 
 """
 
 from .gjfreader import parse_gjf
 
 
-def read_mol(input_file, reader):
+def read_structure(input_file, reader):
 
-    """Reads a molecule from a input file
+    """Reads a structure from a input file
 
     :param input_file: The file name of the input file
     :param reader: A string giving the reader for reading the file
